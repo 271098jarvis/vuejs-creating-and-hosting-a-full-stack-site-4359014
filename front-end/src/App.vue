cb8c7599edb94bdfaae0b482c638a9fb31cedf6f
SBA49 <shikhar.baniya@3ds.com>
2023-12-13 14:52:22 +0530

    Routing in Vue

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Hello World!</h1>
+  <router-view></router-view>
 </template>
 
 <script>
